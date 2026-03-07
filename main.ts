5a4546a7c02e46f0dab4b46a95d587ef5d741f4e
havisvh <havisvh35@gmail.com>
2026-03-07 10:20:57 +0530

    unified command to run both the server and Streaming app

--- main.ts
+++ main.ts
@@ -2,4 +2,25 @@ import { runServer } from "./zener/server.ts";
 
 if (import.meta.main) {
     await runServer();
+    const ionicCommand = Deno.build.os === "windows"
+        ? new Deno.Command("cmd", {
+            args: ["/c", "ionic", "serve","--external"],
+            cwd: "./zenStream",
+            stdout: "inherit",
+            stderr: "inherit",
+        })
+        : new Deno.Command("ionic", {
+            args: ["serve"],
+            cwd: "./zenStream",
+            stdout: "inherit",
+            stderr: "inherit",
+        });
+
+    const child = ionicCommand.spawn();
+    const { code } = await child.status;
+    if (code !== 0) {
+        console.error("Ionic build failed with code:", code);
+    } else {
+        console.log("Ionic build completed successfully.");
+    }
 }
